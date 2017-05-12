ec07e680dae829ba39b33b08c98ad22619a00d09
Mannus Etten <info@therightdirection.nl>
2017-05-12 15:40:49 +0200

    code aangepast vanwege bug #70

---
+++ BikeTouringGIS.Library.UnitTests/GPXFileTest.cs
@@ -0,0 +1,34 @@
+﻿using GPX;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using theRightDirection.Library.UnitTesting;
+using FluentAssertions;
+namespace BikeTouringGISLibrary.UnitTests
+{
+    [TestClass]
+    public class GPXFileTest : UnitTestingBase
+    {
+        [TestMethod]
+        // bug #70 (foute versie, komt door de gpxx extensions tag)
+        public void Load_Incorrect_GPX_no_tracks()
+        {
+            var path = Path.Combine(UnitTestDirectory, "70.gpx");
+            var gpxFile = new GPXFile(path);
+            gpxFile.GetTracks().Count.ShouldBeEquivalentTo(0);
+        }
+
+        [TestMethod]
+        // bug #70 (foute versie, maar de gpxx extensions tag verwijderd voor de track)
+        public void Load_Incorrect_GPX_but_manually_fixed_one_track()
+        {
+            var path = Path.Combine(UnitTestDirectory, "70_2.gpx");
+            var gpxFile = new GPXFile(path);
+            gpxFile.GetTracks().Count.ShouldBeEquivalentTo(1);
+        }
+    }
+} 
