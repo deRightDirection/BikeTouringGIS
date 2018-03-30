0e978e6f22555eb3d3d5f516b5df70136199d346
Mannus Etten <github@therightdirection.nl>
2018-03-30 13:39:03 +0200

    aanpassing onedrive
    #117

--- BikeTouringGISApp/Services/SynchronizingDataService.cs
+++ BikeTouringGISApp/Services/SynchronizingDataService.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WinUX;
+using WinUX.CloudServices.OneDrive;
 
 namespace BikeTouringGISApp.Services
 {
@@ -21,8 +22,19 @@ namespace BikeTouringGISApp.Services
 
         public SynchronizingDataService()
         {
- //           _oneDriveRepository = new OneDriveRepository();
-            _logBooksRepository = SimpleIoc.Default.GetInstance<IRepository<LogBook>>();
+            var oneDrive = new OneDriveService("00000000441DB51C", new string[] { "wl.signin", "wl.offline_access", "onedrive.readwrite", "onedrive.appfolder" });
+            var result = await oneDrive.Connect();
+            if (result.IsConnected)
+            {
+                if (Container == null) { Container = new ServiceContainer(); }
+                var folder = await oneDrive.GetAppRootFolder();
+                var activitiesRepository = new List<Activity>();
+                var settingsRepository = new OneDriveRepository<ApplicationSettings>("settings.json", folder);
+                var gearRepository = new OneDriveRepository<OneDriveGear>("gear.json", folder);
+                var folderRepository = new OneDriveRepository<Activity>("activities.json", folder);
+
+                //           _oneDriveRepository = new OneDriveRepository();
+                _logBooksRepository = SimpleIoc.Default.GetInstance<IRepository<LogBook>>();
             _logsRepository = SimpleIoc.Default.GetInstance<IRepository<Log>>();
         }
 
@@ -90,6 +102,107 @@ namespace BikeTouringGISApp.Services
                         saveToLocal(oneDriveItem);
                         break;
                 }
+            }
+        }
+    }
+
+            private IEnumerable<Activity> _strava;
+            private StravaService _stravaService;
+            private OneDriveRepository<Activity> _repository;
+            private CaloriesCalculator _caloriesCalculator;
+            private List<Activity> _activities;
+            private double _weigth;
+            private bool _newStravaActivities, _newObsoleteActivities;
+
+            public async Task PrepareSynchronisation()
+            {
+                _repository = App.Container.GetInstance<OneDriveRepository<Activity>>();
+                _activities = App.Container.GetInstance<List<Activity>>();
+                _caloriesCalculator = new CaloriesCalculator();
+                _weigth = await ReadWeight();
+            }
+
+            public async Task<IEnumerable<Activity>> ReadActivitiesFromOneDrive()
+            {
+                _activities.Clear();
+                var oneDrive = await _repository.GetEntities();
+                _activities.AddRange(oneDrive);
+                return _activities;
+            }
+
+            internal async Task GetActivityIDs()
+            {
+                _stravaService = await StravaService.GetStravaService();
+                _strava = await _stravaService.GetMyActivityDataAsync(new DateTime(1979, 10, 24));
+            }
+
+            internal IEnumerable<Activity> CountMissingActiviesFromStrava()
+            {
+                var comparer = new ActivityComparer();
+                var result = _strava.Except(_activities, comparer);
+                _newStravaActivities = result.Count() != 0;
+                return result;
+            }
+
+            internal async Task AddActivityToOneDrive(int activityId)
+            {
+                var completeActivity = await _stravaService.GetActivityDataAsync(activityId);
+                if (completeActivity == null)
+                {
+                    return;
+                }
+                var calories = completeActivity.Calories;
+                if (calories == 0)
+                {
+                    calories = _caloriesCalculator.CalculateEnergyUse(completeActivity, _weigth);
+                    completeActivity.Calories = calories;
+                }
+                _activities.Add(completeActivity);
+            }
+
+            internal async Task FinishSynchronisation()
+            {
+                if (_newStravaActivities || _newObsoleteActivities)
+                {
+                    _repository.ClearEntities();
+                    bool isSavedSuccesfully = false;
+                    while (!isSavedSuccesfully)
+                    {
+                        try
+                        {
+                            isSavedSuccesfully = await _repository.AddEntities(_activities);
+                            if (!isSavedSuccesfully)
+                            {
+                                await Task.Delay(2500);
+                            }
+                        }
+                        catch (Exception e) { }
+                    }
+                }
+            }
+
+            private async Task<double> ReadWeight()
+            {
+                var settings = App.Container.GetInstance<OneDriveRepository<ApplicationSettings>>();
+                var configurations = await settings.GetEntities();
+                var config = configurations.FirstOrDefault();
+                return config.Weight;
+            }
+
+            public IEnumerable<Activity> Activities
+            {
+                get
+                {
+                    return _activities;
+                }
+            }
+
+            internal void RemoveObsoleteActivities()
+            {
+                var comparer = new ActivityComparer();
+                var itemsToRemove = _activities.Except(_strava, comparer).ToList();
+                itemsToRemove.ForEach(x => _activities.Remove(x));
+                _newObsoleteActivities = itemsToRemove.Count != 0;
             }
         }
     }
